95a7c8cdcd5a117ce8c16a42e54c3a99ab1f3e4b
Karl Chan <karl-chan@bitbucket.org>
2021-11-14 17:01:46 +0000

    Add cineworld criteria

--- src/main/scala/com/github/karlchan/beatthequeue/merchants/cinema/cineworld/CineworldCriteria.scala
+++ src/main/scala/com/github/karlchan/beatthequeue/merchants/cinema/cineworld/CineworldCriteria.scala
@@ -1,5 +1,18 @@
 package com.github.karlchan.beatthequeue.merchants.cinema.cineworld
 
 import com.github.karlchan.beatthequeue.merchants.Criteria
+import com.github.karlchan.beatthequeue.merchants.Event
 
-case class CineworldCriteria() extends Criteria[Cineworld]
+import java.time.LocalDateTime
+
+case class CineworldCriteria(
+    names: Option[Seq[String]],
+    timeRange: Option[Seq[TimeRange]],
+    venues: Option[Seq[String]],
+    screenTypes: Option[Seq[String]]
+) extends Criteria[Cineworld]
+
+private case class TimeRange(
+    startTime: Option[LocalDateTime],
+    endTime: Option[LocalDateTime]
+)
