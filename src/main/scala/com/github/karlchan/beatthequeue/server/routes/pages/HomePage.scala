c9ddb4f2a1096919c66b80600d40ee63a1950dd9
Karl Chan <karl-chan@bitbucket.org>
2021-12-22 22:35:20 +0000

    Refactor HomePage / LoginPage / RegistrationPage to avoid static import

--- src/main/scala/com/github/karlchan/beatthequeue/server/routes/pages/HomePage.scala
+++ src/main/scala/com/github/karlchan/beatthequeue/server/routes/pages/HomePage.scala
@@ -7,10 +7,11 @@ import com.github.karlchan.beatthequeue.server.routes.pages.templates.widgets._
 import com.github.karlchan.beatthequeue.util.given_Db
 import scalatags.Text.all._
 
-def homePage(authUser: AuthUser): IO[Html] =
-  for {
-    userCriteria <- CriteriaRenderer.forUser(authUser.id)
-  } yield Template.styledPage(
-    navigationBar,
-    userCriteria
-  )
+object HomePage:
+  def render(authUser: AuthUser): IO[Html] =
+    for {
+      userCriteria <- CriteriaRenderer.forUser(authUser.id)
+    } yield Template.styledPage(
+      navigationBar,
+      userCriteria
+    )
