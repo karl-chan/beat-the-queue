28fda33cbc268824354dc1ccddafc199f9b8be09
Karl Chan <karl-chan@bitbucket.org>
2021-12-25 20:12:28 +0000

    Implement event matching logic

--- src/main/scala/com/github/karlchan/beatthequeue/merchants/cinema/cineworld/CineworldCriteria.scala
+++ src/main/scala/com/github/karlchan/beatthequeue/merchants/cinema/cineworld/CineworldCriteria.scala
@@ -2,6 +2,7 @@ package com.github.karlchan.beatthequeue.merchants.cinema.cineworld
 
 import com.github.karlchan.beatthequeue.merchants.Criteria
 import com.github.karlchan.beatthequeue.merchants.Event
+import com.github.karlchan.beatthequeue.util.mapTruthy
 
 import java.time.LocalDateTime
 import java.util.UUID
@@ -15,4 +16,12 @@ final case class CineworldCriteria(
     venues: Option[Seq[String]] = None,
     screenTypes: Option[Seq[String]] = None
 ) extends Criteria[Cineworld]:
-  def matches(event: Event[Cineworld]) = ???
+  def matches(event: Event[Cineworld]) =
+    val CineworldEvent(name, time, venue, screenType) =
+      event.asInstanceOf[CineworldEvent]
+
+    filmNames.mapTruthy(_.contains(name)) &&
+    startTime.mapTruthy(!_.isAfter(time)) &&
+    endTime.mapTruthy(!_.isBefore(time)) &&
+    venues.mapTruthy(_.contains(venue)) &&
+    screenTypes.mapTruthy(_.contains(screenType))
