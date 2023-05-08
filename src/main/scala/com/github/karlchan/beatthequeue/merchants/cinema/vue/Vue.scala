3fe2bb9f525db1553a9c37f47e1d7ed48aad1d09
Karl Chan <karl-chan@bitbucket.org>
2023-05-08 23:29:14 +0100

    Add vue merchant

---
+++ src/main/scala/com/github/karlchan/beatthequeue/merchants/cinema/vue/Vue.scala
@@ -0,0 +1,19 @@
+package com.github.karlchan.beatthequeue.merchants.cinema.vue
+
+import cats.effect.IO
+import cats.implicits._
+import com.github.karlchan.beatthequeue.merchants.Merchant
+import com.github.karlchan.beatthequeue.util.Properties
+import io.circe.generic.auto._
+import io.circe.syntax._
+
+final class Vue extends Merchant[Vue, VueCriteria, VueEvent]:
+  override val name = Vue.Name
+  override val logoUrl =
+    "https://www.myvue.com/assets/images/logos/logo-header-vue.png"
+  override val eventFinder = VueCrawler()
+  override val defaultCriteria = VueCriteria()
+  override val renderer = VueRenderer()
+
+object Vue:
+  val Name = "Vue"
