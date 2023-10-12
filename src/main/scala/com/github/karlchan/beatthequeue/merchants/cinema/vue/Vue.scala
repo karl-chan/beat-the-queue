204094234618d6491df5feb0f9632830fb1a121a
Karl Chan <karl-chan@bitbucket.org>
2023-10-12 09:21:07 +0100

    Fix vue after site change

--- src/main/scala/com/github/karlchan/beatthequeue/merchants/cinema/vue/Vue.scala
+++ src/main/scala/com/github/karlchan/beatthequeue/merchants/cinema/vue/Vue.scala
@@ -9,7 +9,7 @@ import io.circe.syntax._
 final class Vue extends Merchant[Vue, VueCriteria, VueEvent]:
   override val name = Vue.Name
   override val logoUrl =
-    "https://www.myvue.com/assets/images/logos/logo-header-vue.png"
+    "https://www.myvue.com/-/jssmedia/global/img/logo-header-vue.png"
   override val eventFinder = VueCrawler()
   override val criteriaFactory = () => VueCriteria()
   override val renderer = VueRenderer()
--- src/main/scala/com/github/karlchan/beatthequeue/merchants/cinema/vue/VueCrawler.scala
+++ src/main/scala/com/github/karlchan/beatthequeue/merchants/cinema/vue/VueCrawler.scala
@@ -2,21 +2,19 @@ package com.github.karlchan.beatthequeue.merchants.cinema.vue
 
 import java.time.LocalDate
 import java.time.LocalDateTime
-import java.time.LocalTime
 import java.time.Period
 import java.time.format.DateTimeFormatter
 
+import scala.concurrent.duration.DurationInt
+
 import cats.effect.IO
 import cats.syntax.all._
-import cats.syntax.show
 import com.github.karlchan.beatthequeue.merchants.Event
 import com.github.karlchan.beatthequeue.merchants.EventFinder
 import com.github.karlchan.beatthequeue.util.Http
 import com.github.karlchan.beatthequeue.util.Properties
 import com.github.karlchan.beatthequeue.util.given_HttpConnection
 import com.github.karlchan.beatthequeue.util.mapOrTrue
-import com.github.karlchan.beatthequeue.util.shortFormat
-import com.softwaremill.quicklens.modify
 import fs2.Stream
 import io.circe.generic.auto._
 import io.circe.syntax._
@@ -29,42 +27,36 @@ final class VueCrawler(
     untilDate: LocalDate = LocalDate.now.plus(Period.ofYears(1))
 ) extends EventFinder[Vue]:
   private val http =
-    Http(maxParallelism = Properties.getInt("vue.max.parallelism"))
+    Http(
+      maxParallelism = Properties.getInt("vue.max.parallelism"),
+      maxRetries = Properties.getInt("vue.max.retries"),
+      retryDelay = Properties.getInt("vue.retry.delay.ms").milliseconds
+    )
 
   override def run(): Stream[IO, VueEvent] =
-    def listEvents(
-        cinema: LocationsResponse.Cinema,
-        screeningTypeMapping: Map[String, String]
-    ): Stream[IO, VueEvent] =
-      for {
-        filmWithShowings <- Stream.evalSeq(getShowings(cinema.id))
-        showing <- Stream.emits(filmWithShowings.showings)
-        time <- Stream.emits(showing.times)
-        screenTypes = time.screen_type +: time.tags.flatMap(tag =>
-          screeningTypeMapping.get(tag.name)
-        )
-
-        event = VueEvent(
-          name = filmWithShowings.title,
-          time = LocalDateTime
-            .of(
-              LocalDate.parse(showing.date_time),
-              LocalTime.parse(time.time, DateTimeFormatter.ofPattern("h:mm a"))
-            ),
-          venue = cinema.name,
-          screenTypes = screenTypes
-        )
-      } yield event
-
     for {
-      labels <- Stream.eval(getLabels())
-      screeningTypeMapping = labels.screeningtype
-        .map(t => (t.code, t.name))
-        .toMap
+      attributes <- Stream.eval(getAttributes())
+      attributeNames = attributes.toSet
 
-      cinemas <- Stream.evalSeq(getCinemas())
-      event <- listEvents(cinemas, screeningTypeMapping)
-    } yield event
+      cinema <- Stream.evals(getCinemas())
+      filmShowings <- Stream.evals(getShowings(cinema.cinemaId))
+
+      name = filmShowings.filmTitle
+      showingGroup <- Stream.emits(filmShowings.showingGroups)
+      session <- Stream.emits(showingGroup.sessions)
+      time = LocalDateTime.parse(
+        session.showTimeWithTimeZone,
+        DateTimeFormatter.ISO_OFFSET_DATE_TIME
+      )
+      screenTypes = session.attributes
+        .map(_.name)
+        .filter(name => attributeNames.contains(name))
+    } yield VueEvent(
+      name = name,
+      time = time,
+      venue = cinema.cinemaName,
+      screenTypes = screenTypes
+    )
 
   final case class Info(
       names: Seq[String],
@@ -75,100 +67,104 @@ final class VueCrawler(
     for {
       films <- getFilms()
       cinemas <- getCinemas()
-      labels <- getLabels()
+      attributes <- getAttributes()
     } yield Info(
-      names = films.map(_.title),
-      venues = cinemas.map(_.name),
-      screenTypes = labels.screentype
-        .map(_.name)
-        .filter(_ != "All")
-        .sorted ++ labels.screeningtype.map(_.name)
+      names = films.map(_.filmTitle),
+      venues = cinemas.map(_.cinemaName),
+      screenTypes = attributes.sorted.distinct
     )
 
-  private[vue] def getCinemas(): IO[Seq[LocationsResponse.Cinema]] =
+  private[vue] def getCinemas(): IO[Seq[CinemasResponse.Cinema]] =
     for {
-      body <- http.get[LocationsResponse.Body](
-        uri"https://www.myvue.com/data/locations/"
+      body <- http.get[CinemasResponse.Body](
+        uri"https://www.myvue.com/api/microservice/showings/cinemas"
       )
-    } yield body.venues
+    } yield body.result
       .flatMap(_.cinemas)
-      .filter(cinema => cinemaIds.mapOrTrue(_.contains(cinema.id)))
-
-  private[vue] def getShowings(
-      cinemaId: String
-  ): IO[Seq[FilmsWithShowingsResponse.Film]] =
-    for {
-      body <-
-        http.get[FilmsWithShowingsResponse.Body](
-          uri"https://www.myvue.com/data/filmswithshowings/${cinemaId}"
-        )
-    } yield body.films
+      .filter(cinema => cinemaIds.mapOrTrue(_.contains(cinema.cinemaId)))
 
   private[vue] def getFilms(): IO[Seq[FilmsResponse.Film]] =
     for {
       body <-
         http.get[FilmsResponse.Body](
-          uri"https://www.myvue.com/data/films/"
+          uri"https://www.myvue.com/api/microservice/showings/films"
         )
-    } yield body.films
+    } yield body.result
 
-  private[vue] def getLabels(): IO[LabelsResponse.Body] =
-    http.get[LabelsResponse.Body](uri"https://www.myvue.com/data/labels/")
+  private[vue] def getShowings(
+      cinemaId: String
+  ): IO[Seq[ShowingsResponse.Result]] =
+    for {
+      body <-
+        http.get[ShowingsResponse.Body](
+          uri"https://www.myvue.com/api/microservice/showings/cinemas/${cinemaId}/films?minEmbargoLevel=3&includesSession=true&includeSessionAttributes=true"
+        )
+    } yield body.result
 
-private[vue] object LocationsResponse:
+  private[vue] def getAttributes(): IO[Seq[String]] =
+    for {
+      body <-
+        http.get[AttributesResponse.Body](
+          uri"https://www.myvue.com/api/microservice/showings/attributes/showingAttributeGroups"
+        )
+    } yield body.result
+      .find(_.name == "Filter By Screening Type")
+      .get
+      .showingAttributes
+      .map(_.name)
+
+private[vue] object CinemasResponse:
   final case class Body(
-      venues: Seq[Venue]
+      result: Seq[Result]
   )
-  final case class Venue(
+  final case class Result(
       alpha: String,
       cinemas: Seq[Cinema]
   )
   final case class Cinema(
-      id: String,
-      name: String
-  )
-
-private[vue] object FilmsWithShowingsResponse:
-  final case class Body(
-      films: Seq[Film]
-  )
-  final case class Film(
-      id: String,
-      title: String,
-      showings: Seq[Showing]
-  )
-  final case class Showing(
-      date_time: String, // YYYY-MM-DD
-      times: Seq[Time]
-  )
-  final case class Time(
-      time: String, // h:mm a
-      screen_type: String, // "2D" / "3D"
-      tags: Seq[Tag]
-  )
-  final case class Tag(
-      name: String
+      cinemaId: String,
+      cinemaName: String
   )
 
 private[vue] object FilmsResponse:
   final case class Body(
-      films: Seq[Film]
+      result: Seq[Film]
   )
   final case class Film(
-      id: String,
-      title: String
+      filmId: String,
+      filmTitle: String
   )
 
-private[vue] object LabelsResponse:
+private[vue] object ShowingsResponse:
   final case class Body(
-      screeningtype: Seq[ScreeningType],
-      screentype: Seq[ScreenType]
+      result: Seq[Result]
   )
-  final case class ScreeningType(
-      name: String,
-      code: String
+  final case class Result(
+      showingGroups: Seq[ShowingGroup],
+      filmId: String,
+      filmTitle: String
+  )
+  final case class ShowingGroup(
+      date: String, // YYYY-MM-DDTHH:mm:ss
+      sessions: Seq[Session]
+  )
+  final case class Session(
+      attributes: Seq[Attribute],
+      showTimeWithTimeZone: String // YYYY-MM-DDTHH:mm:ssZ
+  )
+  final case class Attribute(
+      name: String
   )
 
-  final case class ScreenType(
+private[vue] object AttributesResponse:
+  final case class Body(
+      result: Seq[Result]
+  )
+  final case class Result(
+      name: String,
+      showingAttributes: Seq[Attribute]
+  )
+
+  final case class Attribute(
       name: String
   )
